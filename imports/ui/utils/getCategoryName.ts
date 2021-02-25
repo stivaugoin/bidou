57556b087d7cfae9d6a3a3d22c93dc5410686684
Steve Bourgoin <stiv.aw@gmail.com>
2021-02-25 07:23:00 -0500

    Create getCategoryName.ts

---
+++ imports/ui/utils/getCategoryName.ts
@@ -0,0 +1,15 @@
+import {
+  CategoriesCollection,
+  CategoryId,
+  ICategory,
+} from "/imports/api/categories";
+
+export function getCategoryName(categoryId: CategoryId): ICategory["name"] {
+  const category = CategoriesCollection.findOne(categoryId);
+
+  if (!category) {
+    return "";
+  }
+
+  return category.name;
+}

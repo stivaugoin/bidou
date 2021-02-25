fa103cdb5e47d6773e6bde02f0dfa63405544dd7
Steve Bourgoin <stiv.aw@gmail.com>
2021-02-25 07:22:25 -0500

    Dashboard page

---
+++ imports/ui/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Dashboard(): JSX.Element {
+  return <div>Dashboard</div>;
+}
---
+++ imports/ui/pages/dashboard/DashboardContainer.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Dashboard } from "./Dashboard";
+
+export function DashboardContainer(): JSX.Element {
+  return <Dashboard />;
+}

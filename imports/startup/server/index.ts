9562086affc1cf28a1c95ad9c8c5dd0190b4bf82
Steve Bourgoin <stiv.aw@gmail.com>
2021-02-28 08:03:35 -0500

    Move Meteor.startup into index file

--- imports/startup/server/index.ts
+++ imports/startup/server/index.ts
@@ -1,4 +1,11 @@
-import "./oauth";
+import { ServiceConfiguration } from "meteor/service-configuration";
 import "./onCreateUser";
 import "./publications";
 import "./validateNewUser";
+
+Meteor.startup(() => {
+  ServiceConfiguration.configurations.upsert(
+    { service: "google" },
+    { $set: Meteor.settings.oauth.google }
+  );
+});

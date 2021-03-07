010b12cdf8a9907b3da883525ae70e0d9a4f0cf3
Steve Bourgoin <stiv.aw@gmail.com>
2021-03-07 15:27:15 -0500

    Remove preflight config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  corePlugins: {
-    // Remove this line to reset basic style
-    preflight: false,
-  },
   purge: ["./imports/ui/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {

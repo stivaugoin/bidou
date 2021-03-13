b8e196063a5f0a5aaced6fa9805bbd221236a7d5
Steve Bourgoin <stiv.aw@gmail.com>
2021-03-13 14:03:38 -0500

    Add styles on Button

--- imports/ui/components/Button/Button.tsx
+++ imports/ui/components/Button/Button.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import React from "react";
 
 interface Props
@@ -9,5 +10,20 @@ interface Props
 }
 
 export function Button({ children, variant, ...rest }: Props): JSX.Element {
-  return <button {...rest}>{children}</button>;
+  return (
+    <button
+      className={classNames(
+        "inline-flex items-center px-5 py-2 border text-base font-medium rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2",
+        {
+          "border-transparent text-white bg-green-600 hover:bg-green-700":
+            variant === "primary",
+          "border-gray-300 text-gray-700 bg-white hover:bg-gray-50":
+            variant === "secondary",
+        }
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }

ffac41cfd316eb8d96c956be3696e47441d22af0
Steve Bourgoin <stiv.aw@gmail.com>
2021-03-14 15:37:30 -0400

    Add styles on login page

--- imports/ui/pages/Login.tsx
+++ imports/ui/pages/Login.tsx
@@ -15,11 +15,16 @@ export function Login(): JSX.Element {
   };
 
   return (
-    <div>
-      <Button onClick={handleClickLoginWithGoogle} variant="primary">
-        Login with Google
-      </Button>
-      {error && <p className="text-red-500 text-xs">{error}</p>}
+    <div className="h-full flex flex-col items-center justify-center bg-gray-50 space-y-16">
+      <img className="h-12 w-auto" src="/images/logo-login.png" />
+
+      <div className="flex flex-col space-y-4">
+        <Button onClick={handleClickLoginWithGoogle} variant="secondary">
+          Login with Google
+        </Button>
+
+        {error && <p className="text-red-500 text-xs">{error}</p>}
+      </div>
     </div>
   );
 }

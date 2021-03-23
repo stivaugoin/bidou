4def9ac10106118913c8a6ec33c5eedc65258528
Steve Bourgoin <stiv.aw@gmail.com>
2021-03-22 20:21:00 -0400

    Create Dashboard

--- imports/ui/pages/dashboard/Dashboard.tsx
+++ imports/ui/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,59 @@
+import classNames from "classnames";
+import dayjs from "dayjs";
 import React from "react";
 import { Page } from "../../components/Page";
+import { UserTotal } from "./DashboardContainer";
+import { formatAmount } from "/imports/utils/formatAmount";
 
-export function Dashboard(): JSX.Element {
+type DepositMonthProps = {
+  month: string;
+  users: Array<UserTotal>;
+};
+
+function Month({ month, users }: DepositMonthProps) {
+  return (
+    <div className="space-y-4">
+      <div className="text-sm font-medium text-gray-500">
+        {dayjs(month).format("YYYY - MMMM")}
+      </div>
+
+      <div
+        className={classNames(
+          "bg-white rounded-lg shadow grid grid-cols-1 gap-4 p-4"
+        )}
+      >
+        {users.map((user) => (
+          <div className="flex items-center justify-between" key={user.userId}>
+            <div className="flex items-center space-x-4 text-sm sm:text-base font-medium text-gray-500 truncate">
+              <img className="h-8 w-8 rounded-full" src={user.picture} />
+              <span>{user.name}</span>
+            </div>
+            <div className="text-base md:text-lg font-medium text-gray-900">
+              {formatAmount(user.total)}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+type Props = { deposits: Array<DepositMonthProps> };
+
+export function Dashboard({ deposits }: Props): JSX.Element {
   return (
     <Page header={{ title: "Dashboard" }}>
-      <>...</>
+      <div className="space-y-4 px-4 md:px-0">
+        <h2 className="text-lg border-b pb-2 leading-6 font-medium text-gray-900">
+          Deposits - Last 3 months
+        </h2>
+
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+          {deposits.map(({ month, users }) => (
+            <Month key={month} month={month} users={users} />
+          ))}
+        </div>
+      </div>
     </Page>
   );
 }
--- imports/ui/pages/dashboard/DashboardContainer.tsx
+++ imports/ui/pages/dashboard/DashboardContainer.tsx
@@ -1,6 +1,78 @@
+import dayjs from "dayjs";
+import { useTracker } from "meteor/react-meteor-data";
 import React from "react";
+import { useCategories } from "../../hooks/useCategories";
 import { Dashboard } from "./Dashboard";
+import { IncomesCollection } from "/imports/api/incomes";
+import { calculateTotalTransactionsAmount } from "/imports/utils/calculateTotalTransactionsAmount";
+import { getTransactionsByMonth } from "/imports/utils/getTransactionsByMonth";
+
+export type UserTotal = {
+  name: string;
+  picture: string;
+  total: number;
+  userId: UserId;
+};
 
 export function DashboardContainer(): JSX.Element {
-  return <Dashboard />;
+  const categories = useCategories(
+    { defaultUserId: { $ne: null }, type: "income" },
+    {
+      fields: {
+        _id: 1,
+        defaultUserId: 1,
+        name: 1,
+      },
+    }
+  );
+
+  const { incomes, users } = useTracker(() => {
+    const minimalDate = dayjs().startOf("day").subtract(3, "month").toDate();
+
+    return {
+      incomes: IncomesCollection.find({ date: { $gte: minimalDate } }).fetch(),
+      users: Meteor.users
+        .find({}, { sort: { "profile.fname": 1, "profile.lname": 1 } })
+        .fetch(),
+    };
+  }, []);
+
+  const transactionsByMonth = getTransactionsByMonth(incomes);
+
+  const deposits = Object.keys(transactionsByMonth).reduce<
+    Array<{
+      month: string;
+      users: Array<UserTotal>;
+    }>
+  >((acc, month) => {
+    const transactions = transactionsByMonth[month];
+
+    const totalByUser = users.reduce<Array<UserTotal>>((accUser, user) => {
+      const userCategory = categories.find(
+        ({ defaultUserId }) => defaultUserId === user._id
+      );
+
+      if (!userCategory) {
+        return accUser;
+      }
+
+      const userTransactions = transactions.filter(
+        ({ categoryId }) => categoryId === userCategory._id
+      );
+
+      return [
+        ...accUser,
+        {
+          name: `${user.profile.fname} ${user.profile.lname}`,
+          picture: user.profile.picture,
+          total: calculateTotalTransactionsAmount(userTransactions),
+          userId: user._id as UserId,
+        },
+      ];
+    }, []);
+
+    return [...acc, { month, users: totalByUser }];
+  }, []);
+
+  return <Dashboard deposits={deposits} />;
 }

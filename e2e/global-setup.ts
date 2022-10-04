4292e9d458cfab625dee68550d3053ad46ec417a
Steve Bourgoin <stiv.aw@gmail.com>
2022-10-04 17:05:34 -0400

    Add logs

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -6,7 +6,11 @@ const credentials = {
 };
 
 async function globalSetup(config: FullConfig) {
+  console.log("globalSetup");
+  // console.log("config", config);
   const [project] = config.projects;
+  // console.log("project", project);
+  console.log(`Running global setup for project ${project.name}`);
   const { storageState, baseURL } = project.use;
 
   const browser = await chromium.launch();
@@ -18,10 +22,12 @@ async function globalSetup(config: FullConfig) {
   await page.click('input[type="password"]');
   await page.fill('input[type="password"]', credentials.password);
   await page.click('text="Sign in with Credentials"');
+  console.log("waiting for page to load");
 
   await page.context().storageState({
     path: storageState as string,
   });
+  console.log("login complete");
 
   await browser.close();
 }

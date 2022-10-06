8f5bc777b627eceb55ae493aa2f647ed3bb294a8
Steve Bourgoin <stiv.aw@gmail.com>
2022-10-05 21:49:29 -0400

    Improve seed database

--- prisma/seed/utils.ts
+++ prisma/seed/utils.ts
@@ -1,3 +1,5 @@
+import data from "./data";
+
 export function randomNumber(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -6,7 +8,11 @@ export function randomValue<T>(array: T[]): T {
   return array[randomNumber(0, array.length - 1)];
 }
 
-export function getRandomDatesByMonth(n: number, month: number, year: number): Date[] {
+export function getRandomDatesByMonth(
+  n: number,
+  month: number,
+  year: number
+): Date[] {
   const dates = [];
   for (let i = 0; i < n; i++) {
     dates.push(new Date(year, month, randomNumber(1, 31)));
@@ -22,4 +28,26 @@ export function isDateBeforeNMonthFromToday(date: Date, n: number): boolean {
 export function dateIsPast(date: Date): boolean {
   const today = new Date();
   return date.valueOf() < today.valueOf();
+}
+
+export function mapExpense(provider: typeof data.providers[number]) {
+  return (date: Date) => {
+    return {
+      amount: randomNumber(
+        provider.expenses.amount[0],
+        provider.expenses.amount[1]
+      ),
+      date,
+      Provider: { connect: { id: provider.id } },
+    };
+  };
+}
+export function mapIncome(category: typeof data.categories[number]) {
+  return (date: Date) => {
+    return {
+      amount: randomValue([100000, 150000, 200000, 250000, 300000]),
+      date,
+      Category: { connect: { id: category.id } },
+    };
+  };
 }

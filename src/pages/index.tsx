99313c70ec0709fb607288c8386dbcdcc5d9153f
Steve Bourgoin <stiv.aw@gmail.com>
2022-10-07 10:38:18 -0400

    Move folders into src folder (#4)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import { Stack, Text } from "@mantine/core";
 import { IconHomeDollar } from "@tabler/icons";
-import { useSession } from "next-auth/react";
 import Head from "next/head";
 import MainLayout from "../components/MainLayout";
 import PageHeader from "../components/PageHeader";
 import { getTitle } from "../utils/getTitle";
 
 export default function Dashboard() {
-  const { data: session } = useSession();
-
   return (
     <>
       <Head>

033058c4f1f1414f049c0e457de768cb66d434c7
Steve Bourgoin <stiv.aw@gmail.com>
2022-10-25 18:51:35 -0400

    Set the size of button to md by default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,6 +35,7 @@ export default function App(
             Button: {
               defaultProps: {
                 radius: "xl",
+                size: "md",
               },
             },
           },

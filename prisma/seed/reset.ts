4bafb7d1f72ee0ea1b143b07a2346f2b1ad8dc11
Steve Bourgoin <stiv.aw@gmail.com>
2022-12-01 11:06:23 -0500

    Upgrade Prisma to 4.7

--- prisma/seed/reset.ts
+++ prisma/seed/reset.ts
@@ -3,9 +3,19 @@ import type { PrismaClient } from "@prisma/client";
 export default async function resetDatabase(db: PrismaClient) {
   console.info(" => Resetting database...");
 
-  await db.expense.deleteMany({});
-  await db.income.deleteMany({});
-  await db.category.deleteMany({});
+  const deleteExpenses = db.expense.deleteMany();
+  const deleteIncomes = db.income.deleteMany();
+  const deleteChildrenCategories = db.category.deleteMany({
+    where: { parentId: { not: null } },
+  });
+  const deleteParentCategories = db.category.deleteMany({});
+
+  await db.$transaction([
+    deleteExpenses,
+    deleteIncomes,
+    deleteChildrenCategories,
+    deleteParentCategories,
+  ]);
 
   console.info(" => Resetting database... [DONE]");
 }

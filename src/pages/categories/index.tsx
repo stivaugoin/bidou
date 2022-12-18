8cd797a11a8eb69d9cf3b21144908900a3058a3d
Steve Bourgoin <stiv.aw@gmail.com>
2022-12-18 18:31:04 -0500

    Page of categories

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,20 +1,29 @@
 import { faFolderTree } from "@fortawesome/free-solid-svg-icons";
-import { Stack } from "@mantine/core";
-import CategoriesList from "../../components/CategoriesList";
-import CreateButton from "../../components/CreateButton";
+import { Button, Collapse, Stack } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { CategoryFormCreate } from "../../components/CategoryFormCreate";
+import { CategoryList } from "../../components/CategoryList";
 import MainLayout from "../../components/MainLayout";
 import PageHeader from "../../components/PageHeader";
 
 export default function Categories() {
+  const [open, { toggle }] = useDisclosure(false);
+
   return (
     <MainLayout>
       <PageHeader icon={faFolderTree} title="Categories">
-        <CreateButton />
+        <Button disabled={open} id="createBtn" onClick={toggle}>
+          Create
+        </Button>
       </PageHeader>
+
+      <Collapse in={open}>
+        <CategoryFormCreate onClose={toggle} />
+      </Collapse>
 
       <Stack spacing="xl">
         {/* TODO: Add filters */}
-        <CategoriesList />
+        <CategoryList />
       </Stack>
     </MainLayout>
   );

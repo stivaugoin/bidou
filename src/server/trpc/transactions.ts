adae83a86ffc2d61aada6484e5166310e1e6f130
Steve Bourgoin <stiv.aw@gmail.com>
2023-01-25 20:27:47 -0500

    Rename transactions by transactionsByMonth

--- src/server/trpc/transactions.ts
+++ src/server/trpc/transactions.ts
@@ -127,7 +127,7 @@ export const transactionRouter = router({
 
       return {
         totalPage,
-        transactions: groupTransactionsByMonth(transactions),
+        transactionsByMonth: groupTransactionsByMonth(transactions),
       };
     }),
 

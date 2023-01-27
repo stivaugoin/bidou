f99c8d07b1f89a8f70b3a05c85f6093e9d5563d7
Steve Bourgoin <stiv.aw@gmail.com>
2023-01-26 20:54:05 -0500

    Delete a category

--- src/lib/notification.tsx
+++ src/lib/notification.tsx
@@ -2,20 +2,23 @@ import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { showNotification } from "@mantine/notifications";
 
-export default function notification(type: "error" | "success") {
+export default function notification(
+  type: "error" | "success",
+  message?: string
+) {
   switch (type) {
     case "error":
       showNotification({
         color: "red",
         icon: <FontAwesomeIcon icon={faTimes} />,
-        message: "Operation failed",
+        message: message || "Operation failed",
       });
       break;
     case "success":
       showNotification({
         color: "teal",
         icon: <FontAwesomeIcon icon={faCheck} />,
-        message: "Operation successful",
+        message: message || "Operation successful",
       });
       break;
   }

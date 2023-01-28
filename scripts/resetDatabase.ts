2c6371bb34e1a1d1863f7df25c7598396032ac97
Steve Bourgoin <stiv.aw@gmail.com>
2023-01-28 18:34:42 -0500

    Reset database before running test e2e

---
+++ scripts/resetDatabase.ts
@@ -0,0 +1,36 @@
+import { exec } from "child_process";
+import { config } from "dotenv";
+import logUpdate from "log-update";
+import { MongoClient } from "mongodb";
+import { getDatabaseName } from "./utils/getDatabaseName";
+
+config();
+
+async function resetDatabase() {
+  const { DATABASE_URL } = process.env;
+
+  if (!DATABASE_URL) throw new Error("DATABASE_URL is not defined");
+
+  const client = new MongoClient(DATABASE_URL);
+  await client.connect();
+
+  const dbName = getDatabaseName(DATABASE_URL);
+  const db = client.db(dbName);
+
+  const collections = await db.collections();
+  for (const collection of collections) {
+    logUpdate(`⏳ Deleting collection ${collection.collectionName}...`);
+    await collection.drop();
+    logUpdate(`✅ Deleted collection ${collection.collectionName}`);
+    logUpdate.done();
+  }
+
+  await client.close();
+
+  logUpdate("⏳ Seeding database...");
+  exec("pnpm prisma db seed");
+  logUpdate("✅ Database seeded");
+  logUpdate.done();
+}
+
+resetDatabase();

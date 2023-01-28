53bc0a87abf5f6ca0eddc1980bed0d9d0f8f4b75
Steve Bourgoin <stiv.aw@gmail.com>
2023-01-27 21:43:20 -0500

    Rename migration

--- scripts/migrateToV4_0_0.js
+++ scripts/migrateToV4_0_0.js
@@ -4,22 +4,20 @@ const { Type } = require("@prisma/client");
 
 dotenv.config();
 
-const {
-  DATABASE_NAME,
-  DATABASE_PRODUCTION_URL,
-  RESET_TRANSACTIONS = false,
-} = process.env;
+const { DATABASE_URL, RESET_TRANSACTIONS = false } = process.env;
 
-if (!DATABASE_NAME || !DATABASE_PRODUCTION_URL) {
+if (!DATABASE_URL) {
   throw new Error("Missing environment variables");
 }
 
-console.info(" == Migrating database to v3.6.0 ==");
+const dbName = DATABASE_URL.split("/").at(-1);
+
+console.info(" == Migrating database to v4.0.0 ==");
 console.info(" -> Converting incomes and expenses to transactions");
 console.info("");
 
 // Connection URL
-const client = new MongoClient(DATABASE_PRODUCTION_URL, {
+const client = new MongoClient(DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -28,7 +26,7 @@ async function main() {
   await client.connect();
   console.info(" > Connected successfully to server");
 
-  const db = client.db(DATABASE_NAME);
+  const db = client.db(dbName);
 
   const collections = {
     expenses: db.collection("Expense"),
@@ -65,7 +63,7 @@ async function main() {
       ` > Creating new transactions... ${transactions.insertedCount} created`
     );
   } catch (error) {
-    console.error(" ERROR: Transactions already migrated");
+    console.error(" X ERROR: Transactions already migrated");
   }
 }
 

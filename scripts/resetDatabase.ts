60eb4c66a1a1f32fd3080b0f733f4883c627bc37
Steve Bourgoin <stiv.aw@gmail.com>
2023-01-29 08:09:26 -0500

    Kill process on error

--- scripts/resetDatabase.ts
+++ scripts/resetDatabase.ts
@@ -7,30 +7,35 @@ import { getDatabaseName } from "./utils/getDatabaseName";
 config();
 
 async function resetDatabase() {
-  const { DATABASE_URL } = process.env;
+  try {
+    const { DATABASE_URL } = process.env;
 
-  if (!DATABASE_URL) throw new Error("DATABASE_URL is not defined");
+    if (!DATABASE_URL) throw new Error("DATABASE_URL is not defined");
 
-  const client = new MongoClient(DATABASE_URL);
-  await client.connect();
+    const client = new MongoClient(DATABASE_URL);
+    await client.connect();
 
-  const dbName = getDatabaseName(DATABASE_URL);
-  const db = client.db(dbName);
+    const dbName = getDatabaseName(DATABASE_URL);
+    const db = client.db(dbName);
 
-  const collections = await db.collections();
-  for (const collection of collections) {
-    logUpdate(`⏳ Deleting collection ${collection.collectionName}...`);
-    await collection.drop();
-    logUpdate(`✅ Deleted collection ${collection.collectionName}`);
+    const collections = await db.collections();
+    for (const collection of collections) {
+      logUpdate(`⏳ Deleting collection ${collection.collectionName}...`);
+      await collection.drop();
+      logUpdate(`✅ Deleted collection ${collection.collectionName}`);
+      logUpdate.done();
+    }
+
+    await client.close();
+
+    logUpdate("⏳ Seeding database...");
+    exec("pnpm prisma db seed");
+    logUpdate("✅ Database seeded");
     logUpdate.done();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
   }
-
-  await client.close();
-
-  logUpdate("⏳ Seeding database...");
-  exec("pnpm prisma db seed");
-  logUpdate("✅ Database seeded");
-  logUpdate.done();
 }
 
 resetDatabase();

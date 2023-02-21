9533a3680897f4afca16e6769bb210bcc945c9a2
Steve Bourgoin <stiv.aw@gmail.com>
2023-02-20 21:10:58 -0500

    Improve backend security

--- src/server/trpc/index.ts
+++ src/server/trpc/index.ts
@@ -1,10 +1,36 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
+import { unstable_getServerSession } from "next-auth";
 import superjson from "superjson";
-import { Context } from "./context";
+import { prisma } from "../../lib/prisma";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
-const t = initTRPC.context<Context>().create({
+export const createContext = async (_opts: CreateNextContextOptions) => {
+  const { req, res } = _opts;
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  return {
+    prisma,
+    session,
+  };
+};
+
+const t = initTRPC.context<Awaited<ReturnType<typeof createContext>>>().create({
   transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
+
+const isAuthenticated = t.middleware(({ ctx, next }) => {
+  if (!ctx.session?.user) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+  return next({
+    ctx: {
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  });
 });
 
 export const router = t.router;
-export const procedure = t.procedure;
+export const procedure = t.procedure.use(isAuthenticated);

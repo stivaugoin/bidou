bd806f286f37379995300c78a43e8a7e96d2464a
Steve Bourgoin <stiv.aw@gmail.com>
2023-12-10 14:07:24 -0500

    Test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,7 +22,7 @@ export default trpc.withTRPC(function App(
   return (
     <>
       <Head>
-        <title>Bidou</title>
+        <title>Bidou!</title>
         <meta name="description" content="Manage your family budget" />
         <link rel="icon" href="/favicon.ico" />
         <meta

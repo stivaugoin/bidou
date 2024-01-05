26a83be2bdbb7e7ef354551a903219b02da8d375
Steve Bourgoin <stiv.aw@gmail.com>
2024-01-05 18:33:23 -0500

    Use AppShell from Mantine

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,15 +1,54 @@
-import { Container } from "@mantine/core";
-import { AppHeader } from "./AppHeader";
+import { AppShell, Burger, Container, Group } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import logoSrc from "../../public/logo-white.png";
+import { AppMenuLinks } from "./AppMenuLinks";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function MainLayout({ children }: Props) {
+  const [opened, { toggle }] = useDisclosure(false);
+  const router = useRouter();
+
   return (
-    <>
-      <AppHeader />
-      <Container pb="xl">{children}</Container>
-    </>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { desktop: true, mobile: !opened },
+      }}
+      padding="lg"
+    >
+      <AppShell.Header>
+        <Container h="100%">
+          <Group h="100%" align="center">
+            <Group justify="space-between" flex={1}>
+              <Image alt="Bidou" src={logoSrc} height={20} width={120} />
+              <Group gap="md" ml="xl" visibleFrom="sm">
+                <AppMenuLinks />
+              </Group>
+            </Group>
+            <Burger
+              hiddenFrom="sm"
+              onClick={toggle}
+              opened={opened}
+              size="sm"
+            />
+          </Group>
+        </Container>
+      </AppShell.Header>
+
+      <AppShell.Navbar py="md" px={4}>
+        <AppMenuLinks mobile />
+      </AppShell.Navbar>
+
+      <AppShell.Main>
+        <Container p={0}>{children}</Container>
+      </AppShell.Main>
+    </AppShell>
   );
 }

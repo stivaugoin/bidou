ffd75540b575e4aa808830c9b3e1d8b8f8c1d407
Steve Bourgoin <stiv.aw@gmail.com>
2024-01-27 21:02:04 -0500

    Cleanup file and add comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,66 +4,50 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
+  let response = NextResponse.next({ request: { headers: request.headers } });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
+        // Get cookie from request object
         get(name: string) {
           return request.cookies.get(name)?.value;
         },
+
+        // Set cookie on both request and response objects
         set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          });
+          request.cookies.set({ name, value, ...options });
           response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
+            request: { headers: request.headers },
           });
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          });
+          response.cookies.set({ name, value, ...options });
         },
+
+        // Remove cookie on both request and response objects
         remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: "",
-            ...options,
-          });
+          request.cookies.set({ name, value: "", ...options });
           response = NextResponse.next({
             request: {
               headers: request.headers,
             },
           });
-          response.cookies.set({
-            name,
-            value: "",
-            ...options,
-          });
+          response.cookies.set({ name, value: "", ...options });
         },
       },
     },
   );
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const { data } = await supabase.auth.getUser();
+  const { user } = data;
 
+  // Redirect to home page if user is logged in and trying to access sign in page
   if (user && request.nextUrl.pathname === "/sign-in") {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
+  // Redirect to sign in page if user is not logged in and trying to access a protected page
   if (!user && request.nextUrl.pathname !== "/sign-in") {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }

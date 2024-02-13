96a5eb7945cc23722ba7aeb59031bb01761b811f
Steve Bourgoin <stiv.aw@gmail.com>
2024-02-12 20:10:11 -0500

    Remove h2

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -10,43 +10,38 @@ export default async function CategoriesPage() {
       <h1>Categories</h1>
       <Link href="categories/new">Create new category</Link>
 
-      <section>
-        <h2>Categories</h2>
-        <table style={{ width: "100%" }}>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Type</th>
-              <th>Sub-categories</th>
-              <th />
+      <table style={{ width: "100%" }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Sub-categories</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {categories?.map((category) => (
+            <tr key={category.id}>
+              <td>
+                <Link href={`/categories/${category.id}`}>{category.name}</Link>
+              </td>
+              <td>{category.type}</td>
+              <td>{category._count.children} sub-categories</td>
+              <td>
+                <form action={deleteCategory}>
+                  <input type="hidden" name="id" value={category.id} />
+                  <button
+                    disabled={category._count.children !== 0}
+                    type="submit"
+                  >
+                    Delete
+                  </button>
+                </form>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {categories?.map((category) => (
-              <tr key={category.id}>
-                <td>
-                  <Link href={`/categories/${category.id}`}>
-                    {category.name}
-                  </Link>
-                </td>
-                <td>{category.type}</td>
-                <td>{category._count.children} sub-categories</td>
-                <td>
-                  <form action={deleteCategory}>
-                    <input type="hidden" name="id" value={category.id} />
-                    <button
-                      disabled={category._count.children !== 0}
-                      type="submit"
-                    >
-                      Delete
-                    </button>
-                  </form>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
